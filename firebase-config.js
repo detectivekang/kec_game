e639aca50fac2da9222bd0c0e5df8c1451bf3e5e
detectivekang <126083968+detectivekang@users.noreply.github.com>
2026-09-06 22:32:34 +0900

    수정
    Updated Firebase SDK version and switched from Analytics to Firestore.

--- firebase-config.js
+++ firebase-config.js
@@ -1,20 +1,18 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyADCFJ4bqLN5F7Ohf8EbNolZ59YiA20zHY",
+const firebaseConfig = {
+    apiKey: "AIzaSyADCFj4bqLN5F7OHf8EbNOlzS9YiA20zHY",
     authDomain: "kec-games.firebaseapp.com",
     projectId: "kec-games",
     storageBucket: "kec-games.firebasestorage.app",
     messagingSenderId: "889557848803",
-    appId: "1:889557848803:web:f6a3fb84a3a83d1a95ebd6",
+    appId: "1:889557848803:web:f6a3fb84a38d1a95ebd6",
     measurementId: "G-752QHXWSNE"
-  };
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+// Firebase 초기화
+const app = initializeApp(firebaseConfig);
+
+// ⚠️ Firestore 데이터베이스 객체 내보내기 (반드시 필요)
+export const db = getFirestore(app);
